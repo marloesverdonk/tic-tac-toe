20c62c85c3f2a4764df915ce2a0bfb47ff81eadc
marloesverdonk <marloesverdonk@live.nl>
2019-09-30 18:35:55 +0200

    Calculate winner

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Game from './components/Game'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+            <h1> Tic Tac Toe </h1>
+        </header>
+        <main>
+        <Game />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
